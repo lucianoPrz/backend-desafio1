34cbee94ac6ddc57fead335218148475395bb8a3
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-11-07 02:19:56 -0300

    se corrigen errores en metodos de la clase ProductManager

--- app.js
+++ app.js
@@ -2,45 +2,64 @@ class ProductManager {
   constructor() {
     this.products = [];
   }
-
+  
+  // retorna todos los productos registrados
   getProducts() {
     return this.products;
   }
 
   //retorna un producto por el id
   getProductById(idProduct) {
+
+    //cónsulta si el prod existe
     let product = this.products.find((prod) => prod.id === idProduct);
 
     if (product) {
       return product;
     } else {
-      console.log("Not found");
+      return "Not found";
     }
   }
 
-  addProduct(title, description, price, thumbnail, codeProducto, stock) {
-    // verifica si el codigo del nuevo prod ya existe
-    let productoRegistrado = this.products.some(
-      (prod) => prod.code === codeProducto
-    );
+  // agrega un nuevo producto a this.products
+  addProduct(title, description, price, thumbnail, code, stock) {
 
-    if (!productoRegistrado) {
+    let id_product = this.products.length;
 
-      let id_product = this.products.length;
-      
-      let product = {
-        title: title,
-        description: description,
-        price: price,
-        thumbnail: thumbnail,
-        code: codeProducto,
-        stock: stock,
-        id_nuevo_producto: ++id_product,
-      };
-
-      this.products.push(product);
-
-      return this.products;
+    // valida que todos los campos requeridos tengan datos
+    if (!title || !description || !price || !thumbnail || !code) {
+      return "Todos los datos son obligatorios"
     }
+
+    // Valida que el code no se encuentre registrado
+    let codeRegistrado = this.products.find(prod => prod.code === code);
+
+    if (codeRegistrado) {
+      return `El codigo: ${code} ya se encuentra registrado.`
+    }
+
+    // crea el obj producto
+    const product = {
+      title: title,
+      description: description,
+      price: price,
+      thumbnail: thumbnail,
+      code: code,
+      stock: stock,
+      id: ++id_product,
+    };
+
+    this.products.push(product);
+
+    return this.products;
   }
 }
+
+
+
+
+
+
+
+
+
